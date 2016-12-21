808564eab889ccfe23153f63db67f72650c8107c
Christian Chung <christian.m.chung@gmail.com>
2016-12-21 12:01:21 -0500

    chore(release): WP 4.7

---
+++ bin/wp-config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+
+if [ -e wp-config-sample.php ]; then
+	sed -i '/WP_DEBUG/a \
+		// Cache all the things \
+		global $memcached_servers; \
+		$memcached_servers = array( \
+			array( \
+				"memcached", \
+				"11211" \
+				) \
+			); \
+		define( "WP_CACHE", true );' wp-config-sample.php
+
+fi
