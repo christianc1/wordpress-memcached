1a58a5885eef296ab48fb16b8ddaee26fca44218
Christian Chung <christian.m.chung@gmail.com>
2017-02-08 16:14:21 -0500

    Get WP_HOME and WP_SITEURL from env vars

--- bin/wp-config.sh
+++ bin/wp-config.sh
@@ -3,14 +3,16 @@ set -euo pipefail
 
 if [ -e wp-config-sample.php ]; then
 	sed -i '/WP_DEBUG/a \
-		// Cache all the things \
-		global $memcached_servers; \
-		$memcached_servers = array( \
-			array( \
-				"memcached", \
-				"11211" \
-				) \
-			); \
-		define( "WP_CACHE", true );' wp-config-sample.php
+// Cache all the things \
+global $memcached_servers; \
+$memcached_servers = array( \
+	array( \
+		"memcached", \
+		"11211" \
+		) \
+	); \
+define( "WP_CACHE", true ); \
+define( "WP_HOME", getenv( "WP_HOME" ) ); \
+define( "WP_SITEURL", getenv( "WP_SITEURL" ) ); \' wp-config-sample.php
 
 fi
